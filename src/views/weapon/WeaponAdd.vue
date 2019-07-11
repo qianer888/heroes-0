93f3fdc3d5304ecbbb61c5ce5d308994cc3b938c
Qianper <1352407360@qq.com>
2019-07-11 10:01:14 +0800

    08-weapon添加功能 添加注释

--- src/views/weapon/WeaponAdd.vue
+++ src/views/weapon/WeaponAdd.vue
@@ -4,12 +4,14 @@
         <form>
           <div class="form-group">
             <label for="name">名称</label>
+            <!-- 注意数据绑定 -->
             <input type="text" class="form-control" id="name" placeholder="名称" v-model="formData.name">
           </div>
           <div class="form-group">
             <label for="power">功能</label>
             <input type="text" class="form-control" id="power" placeholder="功能" v-model="formData.power">
           </div>
+          <!--button自身带有提交功能 所以要阻止默认行为 .prevent 阻止默认行为 -->
           <button @click.prevent='weaponAdd' class="btn btn-success">提交</button>
         </form>
       </div>
@@ -33,6 +35,7 @@ methods:{
             const status = response.status
             if(status === 201) {
                 alert('添加成功')
+                //实现页面跳转
                 this.$router.push('/weapon')
             }else{
                 alert('添加失败')
